9b2dbaf25538ac0cd1c19b4e99f66c4526dfbfb4
Edson Lipa <edson.lipa@gmail.com>
2025-02-04 21:06:19 -0500

    Initial Commit

---
+++ SimpleCats/SimpleCatsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleCatsApp.swift
+//  SimpleCats
+//
+//  Created by Edson Lipa Urbina on 4/02/25.
+//
+
+import SwiftUI
+
+@main
+struct SimpleCatsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SimpleCatsTests/SimpleCatsTests.swift
@@ -0,0 +1,16 @@
+//
+//  SimpleCatsTests.swift
+//  SimpleCatsTests
+//
+//  Created by Edson Lipa Urbina on 4/02/25.
+//
+
+import Testing
+
+struct SimpleCatsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
