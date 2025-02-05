03654282f3740bb3cac014420ce2733611d1cba6
Edson Lipa <edson.lipa@gmail.com>
2025-02-05 01:12:00 -0500

    Improvement: update project structure

---
+++ SimpleCats/Core/Network/NetworkManager.swift
@@ -0,0 +1,12 @@
+//
+//  NetworkManager.swift
+//  SimpleCats
+//
+//  Created by Edson Lipa Urbina on 5/02/25.
+//
+
+import Foundation
+
+class NetworkManager {
+    
+}
---
+++ SimpleCats/Modules/CatList/CatListView.swift
@@ -0,0 +1,18 @@
+//
+//  CatListView.swift
+//  SimpleCats
+//
+//  Created by Edson Lipa Urbina on 5/02/25.
+//
+
+import SwiftUI
+
+struct CatListView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+#Preview {
+    CatListView()
+}
