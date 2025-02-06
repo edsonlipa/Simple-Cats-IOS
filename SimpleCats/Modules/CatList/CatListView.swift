cf801c0acee40ad12390ca7b36f8f1942498d2d1
Edson Lipa <edson.lipa@gmail.com>
2025-02-05 22:40:54 -0500

    Feat: added Cat card

--- SimpleCats/Modules/CatList/CatListView.swift
+++ SimpleCats/Modules/CatList/CatListView.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import Kingfisher
 
 struct CatListView: View {
     @StateObject private var viewModel = CatListViewModel()
@@ -14,7 +15,7 @@ struct CatListView: View {
         List {
             ForEach(viewModel.images, id: \.id) { image in
                 CatImageCell(image: image)
-
+                    .listRowSeparator(.hidden)
             }
             
             if viewModel.isLoading {
@@ -23,13 +24,37 @@ struct CatListView: View {
                     .listRowSeparator(.hidden)
             }
         }
+        .listStyle(.plain)
         .task {
             await viewModel.refresh()
         }
     }
     
+    @ViewBuilder
     func CatImageCell(image: CatImage) -> some View {
-        Text(image.url)
+        if let url = URL(string: image.url) {
+            KFImage(url)
+                .placeholder {
+                    Image("cat-placeholder")
+                        .resizable()
+                        .scaledToFit()
+                        .clipShape(RoundedRectangle(cornerRadius: 12))
+                }
+                .resizable()
+                .roundCorner(radius: .heightFraction(0.05))
+                .scaledToFit()
+                .overlay(alignment: .bottomLeading) {
+                    HStack {
+                        Text("\(image.id)")
+                            .foregroundColor(.white)
+                            .padding(.all, 4)
+                    }
+                    .background(Color.black.opacity(0.5))
+                    .clipShape(RoundedRectangle(cornerRadius: 8))
+                    .padding()
+                }
+                .shadow(radius: 5)
+        }
     }
 }
 
